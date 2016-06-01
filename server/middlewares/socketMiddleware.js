d95a7eec12672bf5cb2b21c795bc991c8f3af43f
Zill Ding <zilldzy@gmail.com>
2016-06-01 16:44:16 +0800

    Add socket server

--- server/middlewares/socketMiddleware.js
+++ server/middlewares/socketMiddleware.js
@@ -1,9 +1,68 @@
+const List = require('immutable').List;
+
+// Global state data
+const rooms = {};
+
+function initRoom(name) {
+  rooms[name] = {
+    playlist: List(), // eslint-disable-line
+    currentPlayingVideoId: '',
+  };
+}
+
+function deleteRoom(name) {
+  delete rooms[name];
+}
+
+// mutate the global state
+function updateData(room, field, data) {
+  rooms[room][field] = data;
+}
+
 function setUpSocket(io) {
   io.on('connection', socket => {
-    console.log('new connection');
+    let room = 'default';
+
+    socket.on('new user', name => {
+      room = name.trim() || room;
+      socket.join(room);
+
+      if (!rooms[room]) {
+        initRoom(room);
+      }
+
+      const { playlist, currentPlayingVideoId } = rooms[room];
+      socket.emit('welcome', {
+        room,
+        playlist: playlist.toArray(),
+        currentPlayingVideoId,
+      });
+    });
+
+    socket.on('action', msg => {
+      io.in(room).emit('action', msg);
+
+      // store on server
+      let field = 'playlist';
+      switch (msg.type) {
+        case 'ADD_VIDEO':
+          return updateData(room, field, rooms[room][field].push(msg.data));
+        case 'DELETE_VIDEO':
+          return updateData(room, field, rooms[room][field].delete(msg.data));
+        case 'PLAY':
+          field = 'currentPlayingVideoId';
+          return updateData(room, field, msg.data);
+        default:
+          return null;
+      }
+    });
 
     socket.on('disconnect', () => {
-      console.log('disconnect');
+      // clean up the room data if all users left
+      const { length = 0 } = io.sockets.adapter.rooms[room] || {};
+      if (length === 0) {
+        deleteRoom(room);
+      }
     });
   });
 }
