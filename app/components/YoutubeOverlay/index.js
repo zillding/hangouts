dd00289d500e6c63b281b8b5e1c63ebc15703162
Zill Ding <zilldzy@gmail.com>
2016-06-02 14:47:36 +0800

    Fix youtube overlay icon style

--- app/components/YoutubeOverlay/index.js
+++ app/components/YoutubeOverlay/index.js
@@ -8,10 +8,18 @@ import React from 'react';
 
 import styles from './styles.css';
 
+const iconStyle = {
+  fontSize: '10rem',
+  paddingTop: '3rem',
+};
+
 function YoutubeOverlay() {
   return (
     <div className={styles.youtubeOverlay}>
-      <i className={`youtube square icon ${styles.icon}`}></i>
+      <i
+        className="youtube square icon"
+        style={iconStyle}
+      ></i>
     </div>
   );
 }
