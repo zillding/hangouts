7ba1673ef261fab7bb1679ac212057297b959f64
Zill Ding <zilldzy@gmail.com>
2016-06-03 15:18:48 +0800

    Fix preview youtube bug
    A known issue: https://github.com/troybetz/react-youtube/issues/74

--- app/components/PreviewYoutubePlayer/index.js
+++ app/components/PreviewYoutubePlayer/index.js
@@ -7,23 +7,22 @@
 import React, { PropTypes } from 'react';
 import YouTube from 'react-youtube';
 
+const opts = {
+  height: '240',
+  width: '400',
+  playerVars: {
+    autoplay: 1,
+  },
+};
 
-function PreviewYoutubePlayer({ videoId }) {
-  const opts = {
-    height: '240',
-    width: '400',
-    playerVars: {
-      autoplay: 1,
-    },
-  };
-
-  return (
+const PreviewYoutubePlayer = ({ videoId }) => (
+  <span>
     <YouTube
       videoId={videoId}
       opts={opts}
     />
-  );
-}
+  </span>
+);
 
 PreviewYoutubePlayer.propTypes = {
   videoId: PropTypes.string.isRequired,
