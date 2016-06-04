a19bf03c8224a1ec6fbfa5932ebc35e0a31ac9f4
Zill Ding <zilldzy@gmail.com>
2016-06-04 16:12:06 +0800

    Use function component for youtube player container

--- app/containers/YoutubePlayer/index.js
+++ app/containers/YoutubePlayer/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
 import YouTube from 'react-youtube';
@@ -14,54 +14,40 @@ import mapDispatchToProps from './actions';
 
 import YoutubeOverlay from 'components/YoutubeOverlay';
 
-class YoutubePlayer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: window.innerWidth > 1800 ? 1280 : 853,
-    };
-  }
+const YoutubePlayer = ({ videoId, onReady, setRTCMute }) => {
+  const [width, height] = window.innerWidth > 1800 ?
+    [1280, 720] : [853, 480];
 
-  render() {
-    const {
-      videoId,
-      onReady,
-      setRTCMute,
-    } = this.props;
-    const { width } = this.state;
-    const height = width === 1280 ? 720 : 480;
+  const containerStyle = {
+    height,
+    width,
+    marginBottom: 10,
+    position: 'relative',
+  };
 
-    const containerStyle = {
-      height,
-      width,
-      marginBottom: 10,
-      position: 'relative',
-    };
+  const opts = {
+    height,
+    width,
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-    const opts = {
-      height,
-      width,
-      playerVars: {
-        autoplay: 1,
-      },
-    };
-
-    return (
-      <div style={containerStyle}>
-        <YouTube
-          videoId={videoId}
-          opts={opts}
-          onReady={e => onReady(e.target)}
-          onPlay={setRTCMute(true)}
-          onPause={setRTCMute(false)}
-          onEnd={setRTCMute(false)}
-          onError={setRTCMute(false)}
-        />
-        {videoId ? null : <YoutubeOverlay />}
-      </div>
-    );
-  }
-}
+  return (
+    <div style={containerStyle}>
+      <YouTube
+        videoId={videoId}
+        opts={opts}
+        onReady={e => onReady(e.target)}
+        onPlay={setRTCMute(true)}
+        onPause={setRTCMute(false)}
+        onEnd={setRTCMute(false)}
+        onError={setRTCMute(false)}
+      />
+      {videoId ? null : <YoutubeOverlay />}
+    </div>
+  );
+};
 
 YoutubePlayer.propTypes = {
   videoId: PropTypes.string.isRequired,
