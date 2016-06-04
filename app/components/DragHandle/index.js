eac4545ad5fe085efb940f22520703cb5db9b67b
Zill Ding <zilldzy@gmail.com>
2016-06-04 21:04:26 +0800

    Fix drag handle style

--- app/components/DragHandle/index.js
+++ app/components/DragHandle/index.js
@@ -11,16 +11,24 @@ import DragHandleIcon from 'material-ui/svg-icons/editor/drag-handle';
 
 const style = {
   position: 'absolute',
-  top: -40,
+  top: -10,
   left: -10,
+  zIndex: 9999,
+  opacity: 0.4,
 };
+
+const handleMouseEnter = e => e.target.style.opacity = 0.9; // eslint-disable-line
+const handleMouseLeave = e => e.target.style.opacity = 0.4; // eslint-disable-line
 
 const DragHandle = () => (
   <IconButton
-    tooltip="Drag Handle"
+    tooltip="Drag Me!"
     tooltipPosition="top-right"
     className="drag handle"
     style={style}
+    disableTouchRipple
+    onMouseEnter={handleMouseEnter}
+    onMouseLeave={handleMouseLeave}
   >
     <DragHandleIcon />
   </IconButton>
