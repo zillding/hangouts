9fdb6d8c726a577303fb8b8f62928c2ea49434a1
Zill Ding <zilldzy@gmail.com>
2016-06-04 17:16:08 +0800

    Modify webrtc drag handle

--- app/components/DragHandle/index.js
+++ app/components/DragHandle/index.js
@@ -6,13 +6,24 @@
 
 import React from 'react';
 
-import styles from './styles.css';
+import IconButton from 'material-ui/IconButton';
+import DragHandleIcon from 'material-ui/svg-icons/editor/drag-handle';
 
-function DragHandle() {
-  return (
-    <i className={`move icon drag handle ${styles.dragHandle}`}>
-    </i>
-  );
-}
+const style = {
+  position: 'absolute',
+  top: -40,
+  left: -10,
+};
+
+const DragHandle = () => (
+  <IconButton
+    tooltip="Drag Handle"
+    tooltipPosition="top-right"
+    className="drag handle"
+    style={style}
+  >
+    <DragHandleIcon />
+  </IconButton>
+);
 
 export default DragHandle;
